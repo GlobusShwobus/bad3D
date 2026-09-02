ee4b3f87fbf8f3c158944aae518b4a762ed6a398
GlobusShwobus <fjodorzaro@gmail.com>
2026-09-02 14:09:46 +0300

    comment

--- EasyDirectXUtils.h
+++ EasyDirectXUtils.h
@@ -43,15 +43,18 @@ Microsoft::WRL::ComPtr<ID3D12Resource> copy_buffer_to_resource_and_get_intermedi
 		D3D12_RESOURCE_STATE_GENERIC_READ
 	);
 
-	// --- CPU copy into the upload resource ---
-	void* mappedPtr = nullptr;
-	D3D12_RANGE readRange{ 0, 0 }; // we don't intend to read from this resource on the CPU
-	intermediary->Map(0, &readRange, &mappedPtr);
+	// map CPU local pointer to the GPU, (with 0 read range)
+	void* CPU_local_pointer = nullptr;
+	D3D12_RANGE read_range{ 0, 0 };
+	intermediary->Map(0, &read_range, &CPU_local_pointer);
 
-	memcpy(mappedPtr, data.data(), num_bytes); // copy my manual data read via cpu onto the buffer
-	intermediary->Unmap(0, nullptr); // release the mappedPtr view thingy
+	// the CPU side pointer and resources internal pointers are mapped together, so now memcpy CPU side mem copies to the resource internal pointer
+	memcpy(CPU_local_pointer, data.data(), num_bytes);
 
-	// --- GPU copy: upload -> default heap ---
+	// unmap the CPU local pointer
+	intermediary->Unmap(0, nullptr); 
+
+	// issue command
 	command_list->CopyBufferRegion(
 		dest.get(),
 		0,
